097bc3bde5f6327c09a5655a324146eb2c112968
David Boothe <davidboothe@gmail.com>
2021-08-03 09:03:43 -0500

    first commit

---
+++ DotNetTest/Controllers/BlogController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DotNetTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public List<Blog> Get()
+        {
+            using var db = new BloggingContext();
+            return db.Blogs
+                .OrderBy(b => b.Url)
+                .ToList();
+        }
+
+        [HttpGet("add-sample")]
+        public void AddSampleBlog()
+        {
+            using var db = new BloggingContext();
+
+            db.Blogs.Add(new Blog {Url = "http://sample.com"});
+            db.SaveChanges();
+        }
+        
+        
+        
+    }
+}
